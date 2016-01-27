2a626da5a92ae05057da72afb158045443d8a92a
Daxiongmao87 <daxiongmao87@gmail.com>
2016-01-27 00:17:08 -0600

    Continued with the level generator.  Almost done with the basic design.  Now that I git, hopefully I won't lose it again! Currently writing the method to detect doors to the next rooms.

--- Disc Warrior/Assets/Scripts/js_level_gen.js
+++ Disc Warrior/Assets/Scripts/js_level_gen.js
@@ -5,6 +5,10 @@ var remaining_rooms : int; //how many rooms are left through the iterations
 var unresolved_rooms : int; //rooms that are remaining after one room fails to be created.  Thrown to the next room successfully created
 var room_width_range : int[]; //0 = min, 1 = max
 var room_height_range : int[]; //0 = min, 1 = max
+
+var obj_floor : GameObject;
+var obj_wall : GameObject;
+var obj_door : GameObject;
 function Start () {
 dungeon_size = new int[2];
 dungeon_size[0] = room_width_range[1]*remaining_rooms;
@@ -45,6 +49,8 @@ var fin_all : boolean = false;
 var check_pos : Vector2;
 var check_length : int;
 var check_width : int;
+var num_pot_doors : int = 0;
+var next_doors : int[];
 do {
     //check left from bottom to top
     if(!fin_left){
@@ -108,15 +114,57 @@ do {
     } 
 fin_all = (fin_left && fin_up && fin_right && fin_down) ? true : false;
 } while (!fin_all);
-print("DONE");
 //then we determine if it was constrained, is it still big enough?
 if((lim_left+1+lim_right) >= room_width*0.5 && (lim_up+1+lim_down) >= room_height*0.5){
 //then we assign values to the grid.  1 = floor, 2 = wall, 3 = door
-    
-}
-
-
-
-
-    
+    room_width = lim_left+1+lim_right;
+    room_height = lim_up+1+lim_down;
+    for(i = -1; i < room_width+1; i++) {
+        for(var j : int = -1; j < room_height+1; j++) {
+            var check_x : int = room_width-lim_left+i;
+            var check_y : int = room_height-lim_down+j;
+                if(i == -1 || i == room_width || j == -1 || j == room_height) {
+                    if(check_x == doorway.x && check_y == doorway.y) dungeon_grid[check_x,check_y] = 3;
+                    else dungeon_grid[check_x,check_y] = 2; 
+                }
+                else dungeon_grid[check_x,check_y] = 1;
+            switch(dungeon_grid[check_x,check_y]) {
+                case 1:
+                    Instantiate(obj_floor,Vector3(check_x,check_y,0),Quaternion.identity);
+                    break;
+                case 2: 
+                    Instantiate(obj_wall,Vector3(check_x,check_y,0),Quaternion.identity);
+                    break;
+                case 3:  
+                    Instantiate(obj_door,Vector3(check_x,check_y,0),Quaternion.identity);
+                    break;                    
+            }
+            //NOW WE GOTTA COUNT THE PLACES TO CONSIDER FOR EXPANDING!!
+            if(num_rooms > 0 && ((i == -1 || i == room_width) && (j > -1 && j < room_height)) || ((j == -1 || j == room_height) && (i > -1 && i < room_width))) {
+                switch(i) {
+                    case -1:
+                        if(dungeon_grid[check_x-1,check_y] == 0) num_pot_doors++;
+                        break;
+                    case room_width:
+                        if(dungeon_grid[check_x+1,check_y] == 0) num_pot_doors++;
+                        break;
+                    }
+                switch(j) {
+                    case -1:
+                        if(dungeon_grid[check_x,check_y-1] == 0) num_pot_doors++;
+                        break;
+                    case room_height:
+                        if(dungeon_grid[check_x,check_y+1] == 0) num_pot_doors++;
+                        break;
+                    }
+                }
+            }
+        }
+    next_doors = new int[num_pot_doors];
+    print(num_pot_doors);
+    for(i = 1 ; i < 2*(room_height-1); i++){
+        for(j = 1; j < 2*(room_width-1); j++){
+            }
+        }
+    }
 }
